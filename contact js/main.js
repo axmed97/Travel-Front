05651d341dfa5905ff68b6b98ab6d90cc0b493f9
axmed97 <indigoboy2012@gmail.com>
2019-12-04 21:33:36 +0400

    Travel Project Home Contact

---
+++ contact js/main.js
@@ -0,0 +1,26 @@
+"use strict"
+
+$(document).ready(function(){
+    $(".basket").click (function(){
+        $(".inbasket").toggleClass("show-basket");
+    });
+    $(".eur").click (function(){
+        $(".drop-ul").toggleClass("show-money");
+    });
+    $(".acc-menu").hide();
+    $(".slide-btn").click(function(){
+        $(this).next().slideToggle();
+    });
+    $(".side-icon").click(function(){
+        $(".side-menu").addClass("active-side-menu")
+    });
+    $(".back-li").click(function(){
+        $(".side-menu").removeClass("active-side-menu")
+    });
+    $("input").click(function(){
+        $(this).toggleClass("active-input")
+    });
+    $(".foot-click").click(function(){
+        $(".foot-menu").toggleClass("d-block")
+    });
+})
